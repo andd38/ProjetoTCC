1b33afd413c2d8b54f4116f21d5a2090fa0e7cbd
MatheusXD1 <108773341+MatheusXD1@users.noreply.github.com>
2023-05-13 13:26:45 -0300

    Carrosel navegação com setas  arrow

--- js/slide.js
+++ js/slide.js
@@ -1,18 +1,35 @@
-var radio = document.querySelector('.manual-bt')
-var cont = 1
+var radio = document.querySelector('.manual-bt');
+var cont = 1;
 
-document.getElementById('radio1').checked = true
+document.getElementById('radio1').checked = true;
 
 setInterval(() => {
-    proximaimg()
-}, 5000)
+    proximaimg();
+}, 5000);
 
-function proximaimg(){
- cont++
+function proximaimg() {
+    cont++;
 
- if (cont > 5){
-    cont = 1
- }
+    if (cont > 5) {
+        cont = 1;
+    }
 
- document.getElementById('radio'+cont).checked = true
+    document.getElementById('radio' + cont).checked = true;
 }
+
+var prevArrow = document.querySelector('.prev-arrow');
+var nextArrow = document.querySelector('.next-arrow');
+
+prevArrow.addEventListener('click', () => {
+    cont--;
+
+    if (cont < 1) {
+        cont = 5;
+    }
+
+    document.getElementById('radio' + cont).checked = true;
+});
+
+nextArrow.addEventListener('click', () => {
+    proximaimg();
+});
