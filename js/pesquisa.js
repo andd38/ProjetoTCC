f1f4ee049594930801de8af565aa8c53d3150144
MatheusXD1 <108773341+MatheusXD1@users.noreply.github.com>
2023-05-27 23:55:27 -0300

    Update  pesquisa js

--- js/pesquisa.js
+++ js/pesquisa.js
@@ -1,16 +1,10 @@
-/* const icone = document.querySelector('#iconeum');
-const conteudoElemento = document.querySelector('.conteudo');
 
-icone.addEventListener('click', function() {
-   conteudoElemento.style.display = "none"; 
-  conteudoElemento.textContent = 'conteudo um';
-}); */
+const iconeum = document.getElementById("iconeum");
 
+iconeum.addEventListener("click", function() {
+const conteudo = document.querySelector(".principal");
+const conteudoum = document.querySelector(".principaldois");
 
-
-var iconbank = document.querySelector('#iconeum');
-var idbank = document.querySelector('.cartas');
-
-iconbank.addEventListener('click',function () {
-    idbank.style.display =
-})
+  conteudo.style.display = "none";
+  conteudoum.style.display = "block";
+}); 
