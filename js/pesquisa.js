d045392efa565570fa775dab8e1ce7c38149dbb6
André Patriarca <57924868+andd38@users.noreply.github.com>
2023-05-28 09:13:54 -0300

    update
    concertei mas tentei fazer os outros deu erro
    
    Co-Authored-By: Denys343454 <130403616+Denys343454@users.noreply.github.com>
    Co-Authored-By: MatheusXD1 <108773341+MatheusXD1@users.noreply.github.com>
    Co-Authored-By: Eduardo Cunha <130495435+SenpaiDEV04@users.noreply.github.com>

--- js/pesquisa.js
+++ js/pesquisa.js
@@ -2,9 +2,58 @@
 const iconeum = document.getElementById("iconeum");
 
 iconeum.addEventListener("click", function() {
-const conteudo = document.querySelector(".principal");
-const conteudoum = document.querySelector(".principaldois");
 
-  conteudo.style.display = "none";
-  conteudoum.style.display = "block";
+  const element = document.querySelector(".adm");
+  element.classList.toggle(".adm");
+
+  
+  if (element.style.display == "block") {
+    element.style.display = "none";
+    
+  } else {
+    element.style.display = "block";
+  }
+  const element1 = document.querySelector(".medicinal");
+  element.classList.toggle(".medicinal");
+
+  
+  if (element1.style.display == "block") {
+    element1.style.display = "none";
+    
+  } else {
+    
+    element1.style.display = "block";
+  }
+
+}); 
+
+const iconedois = document.getElementById("iconedois");
+
+iconedois.addEventListener("click", function() {
+
+
+
+
+  const element2 = document.querySelector(".adm");
+  element2.classList.toggle(".adm");
+  
+  if (element2.style.display == "block") {
+    element2.style.display = "none";
+    
+  } else {
+    
+    element2.style.display = "block";
+  }
+
+  const element3 = document.querySelector(".bancario");
+  element3.classList.toggle(".bancario");
+
+  if (element3.style.display == "block") {
+    element3.style.display = "none";
+    
+  } else {
+    
+    element3.style.display = "block";
+  }
+
 }); 
