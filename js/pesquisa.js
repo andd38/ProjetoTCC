9557abef615a4c9c5faa346c36e5504921b3d9cf
André Patriarca <57924868+andd38@users.noreply.github.com>
2023-05-27 23:27:32 -0300

    update
    Co-Authored-By: Denys343454 <130403616+Denys343454@users.noreply.github.com>
    Co-Authored-By: MatheusXD1 <108773341+MatheusXD1@users.noreply.github.com>
    Co-Authored-By: Eduardo Cunha <130495435+SenpaiDEV04@users.noreply.github.com>

--- js/pesquisa.js
+++ js/pesquisa.js
@@ -1,10 +1,16 @@
-const icone = document.querySelector('#iconeum');
+/* const icone = document.querySelector('#iconeum');
 const conteudoElemento = document.querySelector('.conteudo');
 
 icone.addEventListener('click', function() {
-  conteudoElemento.style.display = "none";
+   conteudoElemento.style.display = "none"; 
   conteudoElemento.textContent = 'conteudo um';
-});
+}); */
 
 
 
+var iconbank = document.querySelector('#iconeum');
+var idbank = document.querySelector('.cartas');
+
+iconbank.addEventListener('click',function () {
+    idbank.style.display =
+})
