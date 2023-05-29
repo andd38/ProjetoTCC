2b47136163fba2872ee0f646442a96ffb84ed1ba
André Patriarca <57924868+andd38@users.noreply.github.com>
2023-05-29 09:50:38 -0300

    o primeiro e segundo
    primeiro e o segundo icone estão funcionando corretamente
    
    Co-Authored-By: MatheusXD1 <108773341+MatheusXD1@users.noreply.github.com>
    Co-Authored-By: Eduardo Cunha <130495435+SenpaiDEV04@users.noreply.github.com>
    Co-Authored-By: Denys343454 <130403616+Denys343454@users.noreply.github.com>

--- js/pesquisa.js
+++ js/pesquisa.js
@@ -1,59 +1,106 @@
 
-const iconeum = document.getElementById("iconeum");
+const iconeum = document.querySelector("#iconeum");
 
 iconeum.addEventListener("click", function() {
 
-  const element = document.querySelector(".adm");
+  const element4 =document.querySelector(".tech");
+  const element5 =document.querySelector(".engenharia");
+  const element6 =document.querySelector(".direito");  
+  const element  =document.querySelector(".adm");
+  const element1 =document.querySelector(".medicinal");
+  
+  
+  element1.classList.toggle(".medicinal");
   element.classList.toggle(".adm");
-
+  element4.classList.toggle(".tech");
+  element5.classList.toggle(".engenharia");
+  element6.classList.toggle(".direito");  
   
-  if (element.style.display == "block") {
-    element.style.display = "none";
-    
-  } else {
-    element.style.display = "block";
-  }
-  const element1 = document.querySelector(".medicinal");
-  element.classList.toggle(".medicinal");
-
   
-  if (element1.style.display == "block") {
+  if (element.style.display == "block" 
+  && element1.style.display == "block" 
+  && element4.style.display == "block" 
+  && element5.style.display == "block"
+  && element6.style.display == "block"   ) {
+
+    element.style.display =  "none";
     element1.style.display = "none";
-    
+    element4.style.display = "none";
+    element5.style.display = "none";
+    element6.style.display = "none";   
+
   } else {
-    
+    element.style.display =  "block";
     element1.style.display = "block";
+     element4.style.display = "block";
+    element5.style.display = "block";
+    element6.style.display = "block";  
   }
+
 
 }); 
 
-const iconedois = document.getElementById("iconedois");
+  const iconedois = document.querySelector("#iconedois");
 
 iconedois.addEventListener("click", function() {
 
 
-
-
   const element2 = document.querySelector(".adm");
-  element2.classList.toggle(".adm");
-  
-  if (element2.style.display == "block") {
-    element2.style.display = "none";
-    
-  } else {
-    
-    element2.style.display = "block";
-  }
-
   const element3 = document.querySelector(".bancario");
+  element2.classList.toggle(".adm");
   element3.classList.toggle(".bancario");
-
-  if (element3.style.display == "block") {
+  
+  if (element2.style.display == "block" && element3.style.display == "block") {
+    element2.style.display = "none";
     element3.style.display = "none";
     
   } else {
     
+    element2.style.display = "block";
     element3.style.display = "block";
   }
 
-}); 
+
+});   
+
+const iconetres = document.querySelector("#iconetres");
+
+iconetres.addEventListener("click", function() {
+
+
+  const element7  =document.querySelector(".adm");
+  const element8  =document.querySelector(".bancario");
+  const element9  =document.querySelector(".engenharia");
+  const element10 =document.querySelector(".direito");  
+  const element11 =document.querySelector(".medicinal");
+
+  element7.classList.toggle(".adm");
+  element8.classList.toggle(".bancario");
+  element9.classList.toggle(".engenharia");
+  element10.classList.toggle(".direito");  
+  element11.classList.toggle(".medicinal"); 
+
+  if (element7.style.display === "block" 
+  && element8.style.display === "block"
+  && element9.style.display === "block" 
+  && element10.style.display === "block"
+  && element11.style.display === "block") {
+
+     element7.style.display = "none";
+     element8.style.display = "none";
+     element9.style.display = "none";
+     element10.style.display = "none";
+     element11.style.display = "none";
+    
+  } else {
+    
+    element7.style.display = "block";
+    element8.style.display = "block";
+    element9.style.display = "block";
+    element10.style.display = "block";
+    element11.style.display = "block";
+
+  }
+
+
+});   
