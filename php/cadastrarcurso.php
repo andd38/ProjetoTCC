aa85859ab70d5f3d9b666b9a75b53f992ffa7344
andd38 <andrepatriarca4@gmail.com>
2023-06-28 15:18:26 -0300

    update cadastrar curso.php
    Co-Authored-By: MatheusXD1 <108773341+MatheusXD1@users.noreply.github.com>
    Co-Authored-By: Eduardo Cunha <130495435+SenpaiDEV04@users.noreply.github.com>

---
+++ php/cadastrarcurso.php
@@ -0,0 +1,91 @@
+ <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>cadastre o seu curso</title>
+    <style>
+        @charset "utf-8";
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+       
+
+        body{
+    background: rgb(83,121,121);
+    background: linear-gradient(17deg, rgba(83,121,121,1) 22%, rgba(21,70,70,1) 83%);
+    background-attachment: fixed;
+    background-size: cover;
+    background-repeat: no-repeat;
+        }
+        *{
+    margin: 0px;
+    padding: 0px;
+    font-family: 'Poppins';
+}
+ img {
+ height: 120px;
+ padding: 0;
+ margin-top:-40px;
+} 
+
+ 
+ header{
+    margin-top: 10px;
+ 
+ }
+ nav{
+    display: flex;
+    justify-content: space-around;
+ }
+ nav>a{
+    color: #fff;
+    text-decoration: none;
+    margin: 15px;
+    font-size: 21px;
+    transition: .3s;
+ }
+h2{
+    color: rgb(83,121,121);
+    text-align: center;
+}
+
+#entrar{
+    border-radius: 25px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    height: 45px;
+    width: 140px;
+    border: none;
+    background-color: #fff;
+    color: #000000;
+}
+#entrar:hover{
+    background-color: rgb(56, 214, 161);
+    outline: none;
+    border: none;
+    transition: 0.5s;
+}
+
+
+  
+  .bx  {
+      width: 25px;
+      font-size: 20px;
+  }
+  </style>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
