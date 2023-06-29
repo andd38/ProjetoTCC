dd019f0e32216041d240b89ae38ca0cdf8968a49
MatheusXD1 <108773341+MatheusXD1@users.noreply.github.com>
2023-06-29 11:12:57 -0300

    Certificado mudanças , só o básico
    Show de bola

--- php/certificado.php
+++ php/certificado.php
@@ -3,7 +3,7 @@ if (isset($_POST['certi'])) {
 session_start();
 require_once('tcpdf/tcpdf.php');
 
-$pdf = new TCPDF();
+$pdf = new TCPDF('P', 'mm', 'A4', true, 'UTF-8');
 
 $pdf->SetCreator('Brasil Concursos');
 $pdf->SetAuthor('');
@@ -11,46 +11,56 @@ $pdf->SetTitle('Certificado de conclusão');
 $pdf->SetSubject('esrer');
 $pdf->SetKeywords('palavra-chave, PDF, PHP');
 
-
-$pdf->AddPage();
-
-
     $id = $_SESSION['id'];
     $nome = $_SESSION['nome'];
     $email = $_SESSION['email'];
+    $dataConclusao =  date("Y/m/d");
+    $curso = 'Astronauta';
+    
+    $pdf->AddPage();
+    
+    $pdf->SetFont('helvetica', 'B', 20);
+    
 
+    $pdf->Cell(0, 10, 'Certificado de Conclusão', 0, 1, 'C');
+    
+    $pdf->SetLineWidth(0.5);
+    $pdf->SetDrawColor(0, 0, 0);
+    
+    
+  
+    $pdf->SetFont('helvetica', '', 14);
+    
 
-$datacertificado = date("Y/m/d"); 
-$nomecurso = 'Técnico em Matemática';
+    $pdf->Cell(0, 20, 'Certificamos que', 0, 1, 'C');
+    $pdf->SetFont('helvetica', 'B', 18);
+    $pdf->Cell(0, 20, $nome, 0, 1, 'C');
+    
+    $pdf->SetFont('helvetica', '', 14);
+    
 
+    $pdf->Cell(0, 10, 'concluiu com sucesso o curso de', 0, 1, 'C');
+    $pdf->SetFont('helvetica', 'B', 16);
+    $pdf->Cell(0, 15, $curso, 0, 1, 'C');
+    $pdf->SetFont('helvetica', '', 14);
+    $pdf->Cell(0, 10, 'em '.$dataConclusao, 0, 1, 'C');
+    
 
-$pdf->SetFont('helvetica', 'B', 24);
+    $pdf->SetFont('helvetica', '', 12);
+    $pdf->MultiCell(0, 10, 'Parabenizamos o aluno por sua dedicação e empenho ao longo do curso, demonstrando um excelente desempenho e comprometimento com o aprendizado. Sua determinação e perseverança são dignas de reconhecimento.', 0, 'J');
 
+    $pdf->MultiCell(0, 10, 'Que este certificado seja um símbolo de sua dedicação e um lembrete constante de que com esforço e perseverança, é possível alcançar grandes realizações.', 0, 'J');
+    
+    $pdf->SetY(-40);
+    
+    $pdf->SetFont('helvetica', 'I', 10);
+    $pdf->Cell(0, 10, 'Data: '.date('d/m/Y'), 0, 0, 'L');
+    
+    $pdf->SetFont('helvetica', 'B', 12);
+    $pdf->Cell(0, 10, 'Assinatura: _______________________________', 0, 1, 'R');
+    
 
-$pdf->Cell(0, 15, 'Certificado', 0, 1, 'C');
-
-$imagePath = 'angolanomatematico.jpg';
-
-$pdf->Image($imagePath, 10, 10, 10, 0);
-
-$pdf->SetFont('Poppins', '', 16);
-
-$pdf->Cell(0, 20, 'Parabéns ' . $nome . ' Por Concluir o curso', 0, 1, 'C');
-$pdf->Cell(0, 10, $nomecurso, 0, 1, 'C');
-
-
-$pdf->SetFont('helvetica', '', 12);
-
-
-$pdf->Cell(0, 20, 'expedido ' . $datacertificado, 0, 1, 'C');
-
-
-$imagem = 'angolanomatematico.jpg';
-$pdf->Image($imagem, 65, 80, 80, 0, 'JPG');
-
-
-$pdf->Output('certificado.pdf', 'I');
-
-
+    $pdf->Output('certificado.pdf', 'I');
+    
 }
 ?>
