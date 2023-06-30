179b5ad6b6db366259e92f694870490b11faedf0
André Patriarca <57924868+andd38@users.noreply.github.com>
2023-06-30 04:30:56 -0300

    update
    Co-Authored-By: MatheusXD1 <108773341+MatheusXD1@users.noreply.github.com>
    Co-Authored-By: Eduardo Cunha <130495435+SenpaiDEV04@users.noreply.github.com>

--- php/aluno.php
+++ php/aluno.php
@@ -23,7 +23,8 @@ if(empty($_SESSION)){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-  
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
    <link rel="stylesheet" href="../css/stylealuno.css">
     <link rel="stylesheet" href="../css/portraitaluno.css" media="screen and (orientation : portrait)">
    
@@ -183,9 +184,7 @@ if(empty($_SESSION)){
                             
                             </form>
                         </div>
-                        <div class="pe"><!-- footer -->
-                            <input type="submit" value="Atulizar" id="atualizar" >
-                        </div>
+                      
 
                     </div>
       </div>
