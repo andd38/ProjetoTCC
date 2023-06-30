e52ccc53575c02e3719f17088a52fea9b9f642e7
André Patriarca <57924868+andd38@users.noreply.github.com>
2023-06-30 04:32:05 -0300

    delete modaal.js
    Co-Authored-By: MatheusXD1 <108773341+MatheusXD1@users.noreply.github.com>
    Co-Authored-By: Eduardo Cunha <130495435+SenpaiDEV04@users.noreply.github.com>

--- php/modal.js
+++
@@ -1,9 +0,0 @@
-const atualizar = document.querySelector(".atualizar");
-const modalcontent = document.querySelector(".modal-content");
-atualizar.addEventListener('click' ,function(){
-    if (modalcontent.style.display === 'block') {
-            modalcontent.style.display == 'block'
-    } else {
-        modalcontent.style.display == 'none';
-    }
-})
